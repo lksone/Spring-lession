bec1cb3a1d355cc32dfc3714c66ab55791f080ca
a-likesheng <13552418058@163.com>
2020-09-08 06:30:13 +0800

    数据添加

--- java-lession/src/main/java/com/demo/string/StringDemo.java
+++ java-lession/src/main/java/com/demo/string/StringDemo.java
@@ -9,10 +9,20 @@ package com.demo.string;
 public class StringDemo {
 
     public static void main(String[] args) {
-        StrMethod1();
+       // StrMethod1();
+      //  strMethod();
+
+        int i = Math.addExact(12, 12);
+        System.out.println(i);
+
+        System.out.println(12^24);
+    }
+
+    private static void strMethod() {
         String msg1 = "123123";
         String msg = new String("123") + new String("123");
         String intern = msg.intern();
+        String.valueOf(12);
         System.out.println(msg1 == intern);
     }
 
