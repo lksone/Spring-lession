371cad984e3b9e99b76a323361906483538b6137
lks <13552418058@163.com>
2020-12-10 22:51:28 +0800

    添加kafka数据信息

---
+++ java-lession/src/main/java/com/demo/leecode/Array2DNumber.java
@@ -0,0 +1,33 @@
+package com.demo.leecode;
+
+/**
+ * 2d array find number
+ */
+public class Array2DNumber {
+
+
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        int i = matrix.length -1, j = 0;
+        while(i<=matrix.length-1 && j>=0){
+            if(matrix[i][j]>target) j--;
+            if(matrix[i][j]<target) j++;
+            if(matrix[i][j]==target){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] array = new int[5][5];
+
+        for(int i = 0; i<=array.length -1;i++){
+           int[] a = new int[5];
+           for (int j = 0; j<= array[0].length -1;j++){
+               a[j] = 6+j;
+               System.out.print(6+j+",");
+           }
+            System.out.println();
+           array[i] = a;
+        }
+    }
+}
