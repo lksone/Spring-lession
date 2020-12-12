b2396bb7dff8cac32ee35709e822e279d1e0dcba
lks <13552418058@163.com>
2020-12-13 00:21:16 +0800

    zk

--- spring-web-demo/src/main/java/com/demo/zookeeper/ZkDemo.java
+++ spring-web-demo/src/main/java/com/demo/zookeeper/ZkDemo.java
@@ -3,10 +3,5 @@ package com.demo.zookeeper;
 public class ZkDemo {
 
     public static void main(String[] args) {
-        
-        ZkClient zkClient = new ZkClient(
-                "192.168.1.3:2181,192.168.1.4:2181,192.168.1.5:2181",
-                20000
-        );
     }
 }
---
+++ zookeeper-demo/src/main/java/com/demo/zookeeper/ZkUtils.java
@@ -0,0 +1,15 @@
+package com.demo.zookeeper;
+
+public class ZkUtils {
+
+    public static void main(String[] args) {
+        Zookeeper
+    }
+
+    private String path = "";
+
+
+    public Boolean checkMainPath(String path){
+
+    }
+}
