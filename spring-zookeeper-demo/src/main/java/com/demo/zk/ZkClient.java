7bc76e87c35458bd9b3f254e3a889d4e416d8c06
a-likesheng <likesheng0925@rayootech.com>
2020-12-14 16:53:44 +0800

    zk

---
+++ spring-zookeeper-demo/src/main/java/com/demo/zk/ZkClient.java
@@ -0,0 +1,112 @@
+package com.demo.zk;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @author lks
+ * @describe zookeeper連接工具類
+ * @date 2020/12/11 15:59
+ **/
+public class ZkClient {
+
+    private final static String ZK_URL = "101.200.235.14:2181";
+
+    private final static int CONNECTION_TIME_OUT= 3*1000;
+
+    private final static int SESSION_TIME_OUT= 3*1000;
+
+    private static CuratorFramework client = null;
+
+
+    /**
+     * 创建地区角色
+     */
+    public static void  createFactory(){
+        RetryPolicy policy = new ExponentialBackoffRetry(1000,3);
+        //連接zookeeper连接数据
+        client = CuratorFrameworkFactory.builder()
+                .connectString(ZK_URL)
+                .connectionTimeoutMs(CONNECTION_TIME_OUT)
+                .sessionTimeoutMs(SESSION_TIME_OUT)
+                .namespace("base")
+                .retryPolicy(policy).build();
+        client.start();
+    }
+
+    /**
+     * 创建节点
+     *
+     * PERSISTENT：永久节点
+     * PERSISTENT_SEQUENTIAL：永久顺序节点
+     * EPHEMERAL：临时节点
+     * EPHEMERAL_SEQUENTIAL：临时顺序节点
+     * CONTAINER：容器
+     * PERSISTENT_WITH_TTL： ttl保持一致
+     * PERSISTENT_SEQUENTIAL_WITH_TTL: ttl永久序列化
+     *
+     * @param path      路径
+     * @param data      存储的数据
+     * @return
+     */
+    public static Boolean createNode(String path,String data){
+        try {
+            client.create().creatingParentsIfNeeded()
+                    .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+                    .forPath(path,data.getBytes());
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 删除节点
+     * @param path          数据
+     * @param versionCode
+     * @return
+     */
+    public static Boolean deleteNode(String path,int versionCode){
+        try {
+            client.delete()
+                    .guaranteed()   //保证强制删除
+                    .deletingChildrenIfNeeded() //递归删除
+                    .withVersion(versionCode)//删除版本
+                    .forPath(path);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 读取数据
+     *
+     * @param path  删除点数据
+     * @return
+     */
+    public static byte[] read(String path){
+        try {
+           return client.getData().forPath(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    /**
+     * 关闭zookeeper连接
+     */
+    public static void close(){
+        client.close();
+    }
+
+}
---
+++ spring-zookeeper-demo/src/main/java/com/demo/zk/ZkDemo.java
@@ -0,0 +1,69 @@
+package com.demo.zk;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.CreateMode;
+
+/**
+ * @author lks
+ * @describe 連接zookeeper數據庫
+ * @date 2020/12/11 11:25
+ **/
+public class ZkDemo {
+
+
+    private final static String ZKURL = "101.200.235.14:2181";
+
+    private final static int CONNECTION_TIME_OUT= 3*1000;
+
+    private final static int SESSION_TIME_OUT= 3*1000;
+
+
+    public static void main(String[] args) throws Exception {
+
+        RetryPolicy policy = new ExponentialBackoffRetry(1000,3);
+        //連接zookeeper连接数据
+        CuratorFramework client = CuratorFrameworkFactory.builder()
+                .connectString(ZKURL)
+                .connectionTimeoutMs(CONNECTION_TIME_OUT)
+                .sessionTimeoutMs(SESSION_TIME_OUT)
+                .retryPolicy(policy).build();
+        //三次
+        client.start();
+        System.out.println(client.getState());
+        /**
+         * 创建一个 允许所有人访问的 持久节点
+         */
+        client.create()
+                //  如果没有父节点，递归创建
+                .creatingParentsIfNeeded()
+                //持久节点
+                .withMode(CreateMode.PERSISTENT)
+                .forPath("/aaa/bbb","dfas".getBytes());
+        //2. 创建永久有序节点
+        client.create()
+                .creatingParentsIfNeeded()
+                .withMode(CreateMode.CONTAINER)
+                .forPath("/bbb","bbbfdsa".getBytes());
+        //3. 创建临时节点
+        client.create()
+                //创建容器节点
+                .creatingParentContainersIfNeeded()
+                // 定时节点
+                .withMode(CreateMode.EPHEMERAL)
+                .forPath("/node012","123".getBytes());
+        //4. 创建临时有序节点
+        client.create()
+                //创建容器节点
+                .creatingParentContainersIfNeeded()
+                // 定时节点
+                .withMode(CreateMode.EPHEMERAL_SEQUENTIAL)
+                .forPath("/node013","123".getBytes());
+
+        client.close();
+
+    }
+}
