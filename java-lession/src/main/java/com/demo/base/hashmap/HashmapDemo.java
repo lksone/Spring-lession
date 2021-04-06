9bfa1d508077a96a5b4a9c6f4d568b7b1a4b9fce
a-likesheng <1056224715@qq.com>
2021-04-06 23:37:12 +0800

    apollo加密

--- java-lession/src/main/java/com/demo/base/hashmap/HashmapDemo.java
+++ java-lession/src/main/java/com/demo/base/hashmap/HashmapDemo.java
@@ -2,24 +2,25 @@ package com.demo.base.hashmap;
 
 import java.math.BigInteger;
 import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author lks
- * @describe todo
+ * @describe
+ * 1、key和value都能够为null
+ * 2、
  * @date 2020/9/4 10:07
  **/
 public class HashmapDemo {
 
     public static void main(String[] args) {
-        Map<String,String> map = new HashMap<>();
-        map.put("12","ljks");
-        map.put("12","ljks");
-        map.put("32","ljks");
+        HashMap<String, String> map = new HashMap<>();
+        map.put(null, null);
+        map.put("12", "ljks");
+        map.put("32", "ljks");
 
         System.out.println(map);
 
-        String a = "-1124497183";
+        String a = "-1";
         BigInteger a1 = new BigInteger(a);
         System.out.println(a1.toString(2));
     }
---
+++ spring-apollo/src/main/java/com/demo/App.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * apollo
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
