4e071e2d051c657990a38439f74e48e33b2d3dc7
a-likesheng <1056224715@qq.com>
2021-04-11 21:06:16 +0800

    补充导入jar

--- spring-zookeeper-demo/src/main/java/com/demo/utils/ThreadPool.java
+++ spring-zookeeper-demo/src/main/java/com/demo/utils/ThreadPool.java
@@ -1,10 +1,52 @@
 package com.demo.utils;
 
+import java.util.concurrent.*;
+
 public class ThreadPool {
 
-    //设置线程池
 
+    private static ExecutorService executorService;
+
+    /**
+     * 核心线程数
+     */
+    private int corePoolSize = 2;
+
+    /**
+     * 最大线程数
+     */
+    private int maximumPoolSize = 4;
+
+    /**
+     * 保存线程存活时间
+     */
+    private long keepAliveTime = 1000;
+
+
+    /**
+     * 线程阻塞队列
+     */
+    private BlockingQueue<Runnable> workQueue = new LinkedBlockingDeque<>();
+
+
+    /**
+     * 当前线程队列
+     */
+    private ThreadPool() {
+        if (executorService == null) {
+            executorService = new ThreadPoolExecutor(corePoolSize, maximumPoolSize, keepAliveTime, TimeUnit.MILLISECONDS, workQueue);
+        }
+    }
+
+    /**
+     * 线程提交过程
+     *
+     * @param o
+     */
     public static void submit(Runnable o) {
+        final Future<?> submit = executorService.submit(o);
+        if (submit != null) {
 
+        }
     }
 }
