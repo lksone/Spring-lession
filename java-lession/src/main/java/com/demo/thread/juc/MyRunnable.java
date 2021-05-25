46c5e234df688c2291fe615928515c7f967c55a5
a-likesheng <1056224715@qq.com>
2021-05-25 10:01:39 +0800

    数据添加

--- java-lession/src/main/java/com/demo/thread/juc/MyRunnable.java
+++ java-lession/src/main/java/com/demo/thread/juc/MyRunnable.java
@@ -1,5 +1,7 @@
 package com.demo.thread.juc;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 public class MyRunnable implements Runnable{
@@ -19,5 +21,6 @@ public class MyRunnable implements Runnable{
         countDown.await();//等待主线程执行完毕，获得开始执行信号...
         System.out.println("处于等待的线程开始自己预期工作......");
         await.countDown();//完成预期工作，发出完成信号...
+        List<String> list = new ArrayList<>();
     }
 }
