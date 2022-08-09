e2a9cff26511832b393b14c41130d5a4ad523600
a-likesheng <1056224715@qq.com>
2022-08-09 09:48:23 +0800

    aspose image的方式和方法

--- spring-repose-demo/src/main/java/com/demo/respose/WorkDocument.java
+++ spring-repose-demo/src/main/java/com/demo/respose/WorkDocument.java
@@ -1,11 +1,13 @@
 package com.demo.respose;
 
 import com.aspose.words.Font;
+import com.aspose.words.Shape;
 import com.aspose.words.*;
 
 import java.awt.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.util.Date;
 
 /**
  * Document文档数据更新
@@ -91,7 +93,7 @@ public class WorkDocument {
     }
 
     /**
-     * 读取文档内容
+     * 读取文档内容，文档的加密功能
      *
      * @throws Exception
      */
@@ -104,8 +106,97 @@ public class WorkDocument {
         in.close();
     }
 
+    /**
+     * 文档加密只能读
+     *
+     * @throws Exception
+     */
+    public static void passwordToWord() throws Exception {
+        Document doc = new Document();
+        DocumentBuilder builder = new DocumentBuilder(doc);
+        builder.write("这是一个测试效果的解密");
+        // 将密码应用于 Word 文档
+        doc.protect(ProtectionType.READ_ONLY, "12345");
+        doc.save("password.docx", SaveFormat.DOCX);
+        System.out.println("Done");
+    }
+
+    /**
+     * 添加评论，这里有问题，如何区分段落问题 Paragraphs
+     *
+     * @throws Exception
+     */
+    public static void addWordComment() throws Exception {
+        Document DocumentForComment = new Document("OutputDocumentWithComments.docx");
+        DocumentBuilder builder = new DocumentBuilder(DocumentForComment);
+
+        // Move the cursor to the beginning of the document for adding comments
+        builder.moveToDocumentStart();
+
+        // 通过提供作者、姓名首字母、时间和评论文本在文档的第一段插入评论
+        Comment comment = new Comment(DocumentForComment, "李科胜", "AW", new Date());
+        //当前段落添加评论
+        builder.getCurrentParagraph().appendChild(comment);
+        //获取段落，什么是段落？
+        comment.getParagraphs().add(new Paragraph(DocumentForComment));
+        comment.getFirstParagraph().getRuns().add(new Run(DocumentForComment, "评论内容，如何添加的呢"));
+        // Save the Document with comments
+        DocumentForComment.save("OutputDocumentWithComments.docx");
+    }
+
+
+    /**
+     * 添加图片的信息，插入图片信息
+     *
+     * @throws Exception
+     */
+    public static void addImageDocument() throws Exception {
+        //添加文档
+        Document AddImagesToWordDOC = new Document("bb.docx");
+        DocumentBuilder imageWriter = new DocumentBuilder(AddImagesToWordDOC);
+        imageWriter.moveToHeaderFooter(HeaderFooterType.HEADER_PRIMARY);
+        //添加图片
+        Shape headerImage = imageWriter.insertImage("123.jpeg");
+        // equals to one inch
+        headerImage.setWidth(1 * 72);
+        headerImage.setHeight(1 * 72);
+        //移动到最后的段落的后面,移动的位置
+        imageWriter.moveTo(AddImagesToWordDOC.getLastSection().getBody().getFirstParagraph());
+        //其他的方法
+        Shape imageAsLinkToFile = imageWriter.insertImage("123.jpeg");
+        imageAsLinkToFile.getImageData().setSourceFullName("123.jpeg");
+        AddImagesToWordDOC.save("Images.docx");
+    }
+
+    /**
+     * 文件位置居中
+     * @throws Exception
+     */
+    public static void addImageDocument2() throws Exception {
+        Document document = new Document();
+        DocumentBuilder builder = new DocumentBuilder(document);
+        //需要添加的形状
+        Shape shape = builder.insertImage("123.jpeg");
+        //设置包裹类型
+        shape.setWrapType(WrapType.NONE);
+        //设置文本后面
+        shape.setBehindText(true);
+        //相对水平的位置
+        shape.setRelativeHorizontalPosition(RelativeHorizontalPosition.COLUMN);
+        //相对垂直位置
+        shape.setRelativeVerticalPosition(RelativeVerticalPosition.MARGIN);
+        //设置水平位置
+        shape.setHorizontalAlignment(HorizontalAlignment.CENTER);
+        //设置垂直位置
+        shape.setVerticalAlignment(VerticalAlignment.CENTER);
+        shape.setWidth(90);
+        shape.setHeight(90);
+        document.save("CreateFloatingPageCenter.docx");
+    }
+
     public static void main(String[] args) throws Exception {
-        documentReadDoc();
+        addImageDocument2();
+        System.out.println("执行完成");
     }
 
 }
