794f7a6f1d18f42464426f8bfb7917aec4216e77
a-likesheng <1056224715@qq.com>
2022-08-09 14:24:45 +0800

    cells jar包引入

--- spring-repose-demo/src/main/java/com/demo/respose/WordWaterMark.java
+++ spring-repose-demo/src/main/java/com/demo/respose/WordWaterMark.java
@@ -18,54 +18,45 @@ public class WordWaterMark {
     /**
      * 插入
      *
-     * @param doc
-     * @param watermarkText
+     * @param doc           document对象
+     * @param watermarkText watermark
      */
     public static void insertWaterMark(Document doc, String watermarkText) {
         //居中,mark的居中效果
-        insertWatermarkText(doc, watermarkText, new Function<Shape, Object>() {
-            @Override
-            public Object apply(Shape watermark) {
-                // Place the watermark in the page center.
-                watermark.setRelativeHorizontalPosition(RelativeHorizontalPosition.PAGE);
-                watermark.setRelativeVerticalPosition(RelativeVerticalPosition.PAGE);
-                watermark.setWrapType(WrapType.NONE);
-                watermark.setVerticalAlignment(VerticalAlignment.CENTER);
-                watermark.setHorizontalAlignment(HorizontalAlignment.CENTER);
-                return null;
-            }
+        insertWatermarkText(doc, watermarkText, watermark -> {
+            // Place the watermark in the page center.
+            watermark.setRelativeHorizontalPosition(RelativeHorizontalPosition.PAGE);
+            watermark.setRelativeVerticalPosition(RelativeVerticalPosition.PAGE);
+            watermark.setWrapType(WrapType.NONE);
+            watermark.setVerticalAlignment(VerticalAlignment.CENTER);
+            watermark.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            return null;
         });
 
         // 顶部，mark
-        insertWatermarkText(doc, watermarkText, new Function<Shape, Object>() {
-            @Override
-            public Object apply(Shape watermark) {
-                watermark.setRelativeHorizontalPosition(RelativeHorizontalPosition.MARGIN);
-                watermark.setRelativeVerticalPosition(RelativeVerticalPosition.MARGIN);
-                watermark.setWrapType(WrapType.NONE);
-                // 垂直线
-                watermark.setVerticalAlignment(VerticalAlignment.TOP);
-                //水平线
-                watermark.setHorizontalAlignment(HorizontalAlignment.CENTER);
-                //  watermark.setTop(120);
-                return null;
-            }
+        insertWatermarkText(doc, watermarkText, watermark -> {
+            watermark.setRelativeHorizontalPosition(RelativeHorizontalPosition.MARGIN);
+            watermark.setRelativeVerticalPosition(RelativeVerticalPosition.MARGIN);
+            watermark.setWrapType(WrapType.NONE);
+            // 垂直线
+            watermark.setVerticalAlignment(VerticalAlignment.TOP);
+            //水平线
+            watermark.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            //  watermark.setTop(120);
+            return null;
         });
 
-        //尾部
-        insertWatermarkText(doc, watermarkText, new Function<Shape, Object>() {
-            @Override
-            public Object apply(Shape watermark) {
-                watermark.setRelativeHorizontalPosition(RelativeHorizontalPosition.MARGIN);
-                watermark.setRelativeVerticalPosition(RelativeVerticalPosition.MARGIN);
-                watermark.setWrapType(WrapType.NONE);
-                // 我们需要自定义距离顶部的高度
-                watermark.setVerticalAlignment(VerticalAlignment.BOTTOM);
-                watermark.setHorizontalAlignment(HorizontalAlignment.CENTER);
-                // 设置距离顶部的高度
-                //   watermark.setTop(480);
-                return null;
-            }
+        /*尾部*/
+        insertWatermarkText(doc, watermarkText, watermark -> {
+            watermark.setRelativeHorizontalPosition(RelativeHorizontalPosition.MARGIN);
+            watermark.setRelativeVerticalPosition(RelativeVerticalPosition.MARGIN);
+            watermark.setWrapType(WrapType.NONE);
+            // 我们需要自定义距离顶部的高度
+            watermark.setVerticalAlignment(VerticalAlignment.BOTTOM);
+            watermark.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            // 设置距离顶部的高度
+            //   watermark.setTop(480);
+            return null;
         });
     }
 
@@ -74,7 +65,7 @@ public class WordWaterMark {
      *
      * @param doc                         document对象
      * @param watermarkText               水印文本
-     * @param watermaskPositionConfigFunc
+     * @param watermaskPositionConfigFunc 水印位置funcation的占位方法
      */
     private static void insertWatermarkText(Document doc, String watermarkText, Function<Shape, Object> watermaskPositionConfigFunc) {
         Shape watermark = new Shape(doc, ShapeType.TEXT_PLAIN_TEXT);
@@ -93,14 +84,15 @@ public class WordWaterMark {
         watermark.setRotation(-20);
         //字体RGB颜色
         final String colorStr = "EE8262";
+        //设置颜色
         watermark.getFill().setColor(new java.awt.Color(Integer.parseInt(colorStr, 16)));
         watermark.setStrokeColor(new java.awt.Color(Integer.parseInt(colorStr, 16)));
         watermaskPositionConfigFunc.apply(watermark);
         Paragraph watermarkPara = new Paragraph(doc);
         watermarkPara.appendChild(watermark);
+        //doc的部分
         for (Section sect : doc.getSections()) {
             insertWatermarkIntoHeader(watermarkPara, sect, HeaderFooterType.HEADER_PRIMARY);
-
         }
     }
 
@@ -113,15 +105,30 @@ public class WordWaterMark {
      */
     private static void insertWatermarkIntoHeader(Paragraph watermarkPara, Section sect, int headerType) {
         //获取到header和尾部的方法
-        HeaderFooter header = sect.getHeadersFooters().getByHeaderFooterType(headerType);
-        if (header == null) {
-            header = new HeaderFooter(sect.getDocument(), headerType);
-            sect.getHeadersFooters().add(header);
-        }
         try {
+            HeaderFooter header = sect.getHeadersFooters().getByHeaderFooterType(headerType);
+            if (header == null) {
+                header = new HeaderFooter(sect.getDocument(), headerType);
+                sect.getHeadersFooters().add(header);
+            }
             header.appendChild(watermarkPara.deepClone(true));
         } catch (Exception e) {
             throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 移除指定的section的水印
+     *
+     * @param section    部分？？待定
+     * @param headerType 类型
+     */
+    public static void removeWatermarkFromHeader(Section section, int headerType) {
+        HeaderFooter headerFooter = section.getHeadersFooters().getByHeaderFooterType(headerType);
+        if (headerFooter != null) {
+            //存在就删除
+            headerFooter.removeAllChildren();
         }
     }
 
