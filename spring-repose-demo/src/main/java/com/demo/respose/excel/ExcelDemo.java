03f12ab6e0495a7345d27c640d3736f0b95def78
a-likesheng <1056224715@qq.com>
2022-08-09 14:47:42 +0800

    word和数据包分类

---
+++ spring-repose-demo/src/main/java/com/demo/respose/excel/ExcelDemo.java
@@ -0,0 +1,18 @@
+package com.demo.respose.excel;
+
+/**
+ *
+ * excel的項目修改和增加
+ *
+ * @author lks
+ * @E-mail 1056224715@qq.com.
+ * @Since 1.0
+ * @Date 2022/8/9 14:44
+ */
+public class ExcelDemo {
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- spring-repose-demo/src/main/java/com/demo/respose/word/WordWaterMark.java
+++ spring-repose-demo/src/main/java/com/demo/respose/word/WordWaterMark.java
@@ -1,4 +1,4 @@
-package com.demo.respose;
+package com.demo.respose.word;
 
 import com.aspose.words.*;
 
